566f880e960a2367b93cfc3b43204f802ffdd2d4
lucivalluz <lucivalsouza@hotmail.com>
2023-11-14 21:01:41 -0300

    DevSorteio

--- devsorteio.js
+++ devsorteio.js
@@ -3,8 +3,15 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+       alert ("O valor minimo deve ser MENOR que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+        alert(result)
+
+
+    }
+
 
 }
